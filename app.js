206f0d97dc519f7bc96c20ddbb9e5b4295ccbce6
BenFrancka <benfrancka@gmail.com>
2021-02-10 21:14:08 -0800

    needs some work

--- app.js
+++ app.js
@@ -3,3 +3,32 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addSolution =document.getElementById('add-solution');
+
+const subNumber1 = document.getElementById('sub-number-1');
+const subNumber2 = document.getElementById('sub-number-2');
+const subButton = document.getElementById('sub-button');
+const subSolution =document.getElementById('sub-solution');
+
+const timesNumber1 = document.getElementById('times-number-1');
+const timesNumber2 = document.getElementById('times-number-2');
+const timesButton = document.getElementById('times-button');
+const timesSolution = document.getElementById('times-solution');
+
+const divideNumber1 = document.getElementById('divide-number-1');
+const divideNumber2 = document.getElementById('divide-number-2');
+const divideButton = document.getElementById('divide-button');
+const divideSolution = document.getElementById('divide-solution');
+
+
+
+
+
+
+
+
+
